5676dd9e163864beda4d36c5817d385459094abb
bradives <52189225+bradives@users.noreply.github.com>
2019-12-04 13:54:40 -0800

    trying something else

--- week-08/fd05-site/scripts/includes.js
+++ week-08/fd05-site/scripts/includes.js
@@ -29,11 +29,3 @@ function includeHTML() {
       }
     }
   }
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-    x.className += " responsive";
-    } else {
-    x.className = "topnav";
-    }
-    }
