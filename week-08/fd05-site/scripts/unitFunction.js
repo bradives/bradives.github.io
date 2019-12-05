1bd05793855184327da06892e88365b2f85e0bdc
bradives <52189225+bradives@users.noreply.github.com>
2019-12-05 08:42:32 -0800

    maybe this will be cool

---
+++ week-08/fd05-site/scripts/unitFunction.js
@@ -0,0 +1,10 @@
+
+        function unitFunction() {
+          var x = document.getElementById("unitTopnav");
+          if (x.className === "unittopnav") {
+            x.className += " responsive";
+          } else {
+            x.className = "unittopnav";
+          }
+        }
+        
